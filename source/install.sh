df278f298136794816b5680283a46e522bd05e27
Alexander Koch <akmail@web.de>
2022-02-08 17:08:14 +0100

    Update install.sh
    changed target path to /var/log/monitor/pi_net/

--- source/install.sh
+++ source/install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root, e.g. 'sudo ./install.sh'"
   exit 0
@@ -44,20 +43,20 @@ else
     cp styles.css /var/lib/monitor_pi_net/
 fi
 
-echo "creating target report directory /media/monitor_pi_net/ and mounting it as a ramdisk in /etc/fstab ..."
-mkdir -p /media/monitor_pi_net
-chmod a+rwx /media/monitor_pi_net
-if [ ! -f "/etc/fstab.bak_monitor_pi_net" ]; then
-    sudo cp /etc/fstab /etc/fstab.bak_monitor_pi_net
-fi
-grep monitor_pi_net /etc/fstab || sudo echo "tmpfs     /media/monitor_pi_net   tmpfs   size=2M,noatime   0     0" >> /etc/fstab
-sudo mount -a
+# echo "creating target report directory /var/log/monitor_pi_net/ and mounting it as a ramdisk in /etc/fstab ..."
+# mkdir -p /var/log/monitor_pi_net
+# chmod a+rwx /var/log/monitor_pi_net
+# if [ ! -f "/etc/fstab.bak_monitor_pi_net" ]; then
+#     sudo cp /etc/fstab /etc/fstab.bak_monitor_pi_net
+# fi
+# grep monitor_pi_net /etc/fstab || sudo echo "tmpfs     /media/monitor_pi_net   tmpfs   size=2M,noatime   0     0" >> /etc/fstab
+# sudo mount -a
 
 if [ -d "/var/www/html" ]; then
-    echo "found /var/www/html, creating link to /media/monitor_pi_net ..."
-    ln -s /media/monitor_pi_net /var/www/html/monitor_pi_net
+    echo "found /var/www/html, creating link to /var/log/monitor_pi_net ..."
+    ln -s /var/log/monitor_pi_net /var/www/html/monitor_pi_net
 else
-    echo "no /var/www/html directory found. Please add html report /media/monitor_pi_net manually to your web server!"
+    echo "no /var/www/html directory found. Please add html report /var/log/monitor_pi_net manually to your web server!"
 fi
 
 echo "enabling service monitor_pi_net ..."
